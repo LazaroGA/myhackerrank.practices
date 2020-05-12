7f198b5a4ac7daa35aa45ededd1833e08c2820d7
Adan Lazaro <adan.lazaro@gmail.com>
2020-05-12 00:27:23 -0500

    Added  day 7 of 30 days of code and projects rename

---
+++ 30Days.Day7.Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp));
+
+        var space = " ";
+
+        for (int i = arr.Length - 1; i >= 0; i--)
+        {
+            if (i== 0)
+            {
+                space = string.Empty;
+            }
+            Console.Write(arr[i] + space);
+        }
+    }
+}
