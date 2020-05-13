a74f60a36fa5e6019f3245957eb47d97e1415a94
Adan Lazaro <adan.lazaro@gmail.com>
2020-05-12 23:01:23 -0500

    Agregado dia 8 de 30 dias de codigo

---
+++ 30Days.Day8.DictionariesAndMaps/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+class Solution
+{
+    static void Main(String[] args)
+    {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution */
+        int entries = int.Parse(Console.ReadLine());
+
+        Dictionary<string, string> phonebook = new Dictionary<string, string>();
+
+        for (int i = 0; i < entries; i++)
+        {
+            string[] entry = Console.ReadLine().Split(' ');
+            phonebook.Add(entry[0], entry[1]);
+        }
+
+        List<string> namesQ = new List<string>();
+        string tempQ = Console.ReadLine();
+        while (!string.IsNullOrEmpty(tempQ))
+        {
+            namesQ.Add(tempQ);
+            tempQ = Console.ReadLine();
+        };
+
+        foreach (var itemName in namesQ)
+        {
+            if (phonebook.ContainsKey(itemName))
+            {
+                Console.WriteLine($"{itemName}={phonebook[itemName]}");
+            }
+            else
+            {
+                Console.WriteLine("Not found");
+            }
+        }
+
+    }
+}
