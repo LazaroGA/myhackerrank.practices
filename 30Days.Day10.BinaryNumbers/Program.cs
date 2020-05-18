840a4c71ef9570fb7772a7742b1cc7cb5ebddb3c
Adan Lazaro <adan.lazaro@gmail.com>
2020-05-18 00:58:34 -0500

    Agregados dias 9 y 10 de 30 disa de codigo

---
+++ 30Days.Day10.BinaryNumbers/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System;
+
+class Solution
+{
+    static void Main(string[] args)
+    {
+        int n = Convert.ToInt32(Console.ReadLine());
+        Stack<int> binary_ = new Stack<int>();
+        int remainder;
+
+        while (n > 0)
+        {
+            remainder = n % 2;
+            binary_.Push(remainder);
+            n = (n / 2);
+        }
+
+        int lastCounter = 0;
+        List<int> continuousRecord = new List<int>();
+
+        foreach (var item in binary_)
+        {
+            if (item == 1)
+            {
+                lastCounter++;
+            }
+            else
+            {
+                lastCounter = 0;
+            }
+            continuousRecord.Add(lastCounter);
+            //Console.Write($" item:{item}");
+            //Console.Write($"-counter:{lastCounter}");
+            //Console.Write("\n");
+        }
+
+        //Console.Write("\n");
+
+        //foreach (var item in binary_)
+        //{
+        //    Console.Write(item);
+        //}
+
+        //Console.Write("\n");
+
+        //foreach (var item in continuousRecord)
+        //{
+        //    Console.Write(item);
+        //}
+
+        Console.WriteLine(continuousRecord.Max());
+    }
+}
---
+++ 30Days.Day9.Recursion3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.IO;
+using System;
+using System.Runtime.CompilerServices;
+
+class Solution
+{
+
+    // Complete the factorial function below.
+    static int factorial(int n)
+    {
+        Console.Write($"\nn: {n} \t n > 1 : {n > 1}");
+        if (n > 1)
+        {
+            Console.Write($"\texecuting: {n} * factorial({n-1})");
+            return n * factorial(n - 1);
+        }
+        Console.Write($"\treturning: {n}");
+        return n;
+    }
+
+    static void Main(string[] args)
+    {
+        //TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+        TextWriter textWriter = new StreamWriter(Path.Combine(@"C:\A\hackerrank", "factorial_output.txt"), true);
+        StreamReader reader = new StreamReader(Path.Combine(@"C:\A\hackerrank", "factorial_input.txt"), true);
+
+
+        int n = Convert.ToInt32(reader.ReadLine());
+
+        Console.WriteLine($"Starting...");
+        Console.WriteLine($"\nGet {n}!");
+
+        int result = factorial(n);
+        Console.WriteLine($"\n\nResult: {result}");
+        Console.WriteLine($"\nFinished!");
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
